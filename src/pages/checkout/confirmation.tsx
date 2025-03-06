b8f9244c7310d5fea74aa803d639747b0128a82a
Mohamad-Kahil <mkahil@diegypt.com>
2025-03-06 09:24:30 +0000

    improvement 1

---
+++ src/pages/checkout/confirmation.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import OrderConfirmationPage from "@/components/checkout/OrderConfirmationPage";
+
+const OrderConfirmationRoute = () => {
+  return <OrderConfirmationPage />;
+};
+
+export default OrderConfirmationRoute;
---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CheckoutPage from "@/components/checkout/CheckoutPage";
+
+const CheckoutPageRoute = () => {
+  return <CheckoutPage />;
+};
+
+export default CheckoutPageRoute;
